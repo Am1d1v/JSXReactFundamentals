732718d7ae3ad31af12ff80ca184fbdffb3c2ba6
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-28 22:14:58 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+      <p>Template</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
