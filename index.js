984e3aed940ff9af8ca07caf48f6fcd7882d1b81
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-28 22:54:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 
+const Book = (props) => {
+  console.log(props);
+
+  return React.createElement('div', {}, [
+    React.createElement('h2', {}, props.name),
+    React.createElement('p', {}, props.year),
+    React.createElement('p', {}, props.price)
+  ])
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Book name="Name1" year="2010" price={1000} />
+    <Book name="Name1" year="2010" price={1000} />
+    <Book name="Name1" year="2010" price={1000} />
   </React.StrictMode>
 );
 
